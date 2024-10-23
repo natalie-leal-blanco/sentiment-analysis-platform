b97079877c0e5e9c03e2a4a9b59d476424c85f89
natalie-leal-blanco <natalie.lealblanco.24@gmail.com>
2024-10-23 01:52:05 -0500

    Create schemas.py

---
+++ app/models/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+class SentimentRequest(BaseModel):
+    texts: List[str]
+
+class SentimentResponse(BaseModel):
+    sentiments: List[Dict[str, float]]
+    processing_time: float
