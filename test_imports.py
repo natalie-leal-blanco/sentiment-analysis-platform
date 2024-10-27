26b5a2289321dc9ba8cc227c4e90d0cbcefcd727
Natalie Leal Blanco <natalie.lealblanco24@gmail.com>
2024-10-27 10:43:27 -0500

    Updates

--- test_imports.py
+++
@@ -1,8 +0,0 @@
-import fastapi
-import pydantic
-import uvicorn
-
-print("Package versions:")
-print(f"FastAPI: {fastapi.__version__}")
-print(f"Pydantic: {pydantic.__version__}")
-print(f"Uvicorn: {uvicorn.__version__}")
